cd78302e531880e188e45f93de0c6b4c85da3e86
Saurabh-Singh-Negi <saurabhnegi0007@gmail.com>
2022-04-07 11:07:17 +0530

    logic to check lucky or not

--- script.js
+++ script.js
@@ -4,7 +4,31 @@ const checkNumber = document.querySelector("#check-number");
 const displayMessage = document.querySelector("#display-message");
 
 function checkBirthdayIsLucky() {
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    if(sum && luckyNumber.value) {
 
+        if(sum % luckyNumber.value === 0){
+            displayMessage.innerText = "It's a lucky one";
+        }
+        else {
+            displayMessage.innerText = "Not a lucky one";
+        }
+    }
+    else {
+        displayMessage.innerText = "enter both the fields";
+    }
+}
+
+function calculateSum(dob) {
+    var NewDob = dob.replaceAll("-","");
+    var sumOfDigits = 0;
+    for(let i=0;i<NewDob.length;i++) {
+        sumOfDigits = sumOfDigits + Number(NewDob.charAt(i));
+    }
+    
+    return sumOfDigits;
+ 
 }
 
 
