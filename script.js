512b0571e11aae4aed056dbbd81dc59b53aeea34
Saurabh-Singh-Negi <saurabhnegi0007@gmail.com>
2022-04-07 08:35:54 +0530

    added control for html elements to JS

---
+++ script.js
@@ -0,0 +1,11 @@
+const dateOfBirth = document.querySelector("#dob");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumber = document.querySelector("#check-number");
+const displayMessage = document.querySelector("#display-message");
+
+function checkBirthdayIsLucky() {
+
+}
+
+
+checkNumber.addEventListener("click", checkBirthdayIsLucky);
